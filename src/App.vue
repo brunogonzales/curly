7e56897ade1b9b9a8b34ccedcf569b4b9e31be40
Bruno Gonzales <57231813+brunogonzales@users.noreply.github.com>
2021-01-29 09:09:24 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <main>
+    <h2>Banned ingredients for curly hair 👩🏻‍🦱</h2>
+    <input type="text" v-model="query" placeholder="Search ingredient" />
+    <div v-for="ingredient in filteredIngredients" :key="ingredient">
+      {{ ingredient }}
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ingredients: [
+        "AD alcohol 40",
+        "Alcohol Denat",
+        "Alkyl benzene sulfonate",
+        "Alkylbenzene sulfonate",
+        "Petroleum Sodium carboxylate",
+      ],
+      query: "",
+    };
+  },
+
+  computed: {
+    filteredIngredients() {
+      if (!this.query) return this.ingredients;
+
+      return this.ingredients.filter((ingredient) =>
+        ingredient.toLowerCase().includes(this.query.toLowerCase())
+      );
+    },
+  },
+};
+</script>
+
+<style>
+</style>
